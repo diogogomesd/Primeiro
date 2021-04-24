cddbc1572e2d5ac2f447a5512015b6c946f466e6
diogo <diogo@10.0.0.101>
2021-04-23 21:32:30 -0400

    criação do projeto

---
+++ Primero/src/Hello/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Hello;
+
+/**
+ * @author diogo
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("olá mundo");
+		int n1 = 123;
+		int n2 = 123;
+		
+		int res = (n1 * n2)/2;
+		
+		System.out.println(res);
+	}
+
+}
---
+++ Primero/src/module-info.java
@@ -0,0 +1,2 @@
+module Primero {
+}
